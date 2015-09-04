3d17bcc7fdb8cebdcf6e8155242ee09ee7f512f2
Wilson Liao <droyuki@gmail.com>
2015-09-04 15:45:28 +0800

    Fix spark start script

--- BigBoost/docker/Spark_docker/scripts/start_spark.sh
+++ BigBoost/docker/Spark_docker/scripts/start_spark.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -x
 (
-    
 # start ssh
 service sshd start
 
 # start spark
-/usr/local/spark/sbin/start-all.sh
+/spark-1.4.1-bin-hadoop2.6/sbin/start-master.sh
+/spark-1.4.1-bin-hadoop2.6/sbin/start-slave.sh spark://bigboost-spark:7077
 
 # execute a foreground program
 while true; do sleep 1000; done
