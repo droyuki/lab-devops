8fc73625c2412dfcb1e431527d0fe5c7a12ef072
Wilson Liao <droyuki@gmail.com>
2015-09-30 20:59:52 +0800

    [R]Add streaming read line

--- BigBoost/R/getRDD.R
+++ BigBoost/R/getRDD.R
@@ -1,38 +1,8 @@
 #!/usr/bin/env Rscript
 stime <- Sys.time()
 stimefmt <- format(stime, "%Y_%m_%d_%H%M")
-setwd("./")
-source("ser_helpers.R")
-
-## get command line argument(s)
-cmdargs <-  getCommandLineArg()
-if ( cmdargs["data_typeid"] == '1' ) {
-    sec <- "ser1"
-} else if ( cmdargs["data_typeid"] == '2' ) {
-    sec <- "ser2"
-}
-
-## read config
-conf_fname <- "conf.ini"
-if ( file.exists(conf_fname) ) {
-    config <- readIniConfig(conf_fname)
-} else {
-    message('Configure file not found. Program aborted.')
-    quit(status=1)
-}
-log_path <- config[[sec]]["log_path"]
-
-
-## start logging
-logfile <- file(file.path(log_path,  "stderr.log"), open='a')
-sink(logfile, append=TRUE, type='message')
-
-message('')
-message("Job starting time: ", stime)
-message("Rscript run on ", system("uname -n", intern=TRUE))
-message('Config file read as:')
-write.table(as.data.frame(config$ser1), quote=FALSE, sep='\t', col.names=FALSE, file=stderr())
-
+setwd("/opt/BigBoost/R/")
+source("getRDD_helper.R")
 ## load libraries
 suppressPackageStartupMessages({
     library(methods)
@@ -43,16 +13,9 @@ suppressPackageStartupMessages({
     library(pscl)
 })
 
-
 ## get streaming lines
 input <- "stdin"
-newdata <- getStreamingLines(input, c("ts", "acct", "cnt"))
-newdata <- newdata[, `:=`(ts=as.integer(ts),
-                          cnt=as.integer(cnt))]
-newdata <- newdata[!grepl("\\$$", acct)] # restrict to non-computer accounts
-message("New data loaded. Number of lines: ", nl <- nrow(newdata))
-
-if ( nl == 0 ) {
-    message("No new coming data.")
-    quit(status=0)    ### early exit due to no new coming data
-}
+newdata <- getStreamingLines(input)
+workerName <- system("uname -n", intern=TRUE)
+cat("Rscript run on ",workerName,"\n")
+cat(newdata)
--- BigBoost/R/getRDD_helper.R
+++ BigBoost/R/getRDD_helper.R
@@ -4,35 +4,10 @@ suppressPackageStartupMessages({
     library(compiler)
 })
 
-getCommandLineArg <- function() {
-    cmdargs <- commandArgs()
-    scriptname <- gsub("--file=", '', grep("--file=", cmdargs, value=TRUE))
-    if ( !"--args" %in% cmdargs ) {
-        message(sprintf("Usage: %s [1|2]", scriptname))
-        quit(status=1)
-    }
-    args <- cmdargs[-(1:which(cmdargs == "--args"))]
-    if ( !args[1] %in% as.character(1:2) )
-        stop("Command line arg not in pre-defined set.")
-    names(args) <- "data_typeid"
-    args
-}; getCommandLineArg <- cmpfun(getCommandLineArg, options=list(suppressUndefined=TRUE))
-
-getStreamingLines <- function(input="stdin", cols) {
-    require(data.table)
+getStreamingLines <- function(input="stdin") {
     f <- file(input)
     open(f)
     on.exit(close(f))
-    ncol <- length(cols)
-    res <- list()
-    for ( i in 1:ncol )
-        res[[i]] <- character()
-    while( length(line <- readLines(f, n=1)) > 0 ) {
-        lineparsed <- unlist(strsplit(line, ','))
-        for ( i in 1:ncol )
-            res[[i]] <- c(res[[i]], lineparsed[i])   
-    }
-    res <- as.data.table(res)
-    setnames(res, cols)
-    res
+    line <- readLines(f, n=1)
+    line
 }; getStreamingLines <- cmpfun(getStreamingLines, options=list(suppressUndefined=TRUE))
