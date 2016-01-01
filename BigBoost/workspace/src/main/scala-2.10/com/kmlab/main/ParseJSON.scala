56847ee0c0a14bee12e32c84e0005c66df0fec46
Wilson Liao <droyuki@gmail.com>
2016-01-01 18:23:20 +0800

    add instruction.

--- BigBoost/workspace/src/main/scala-2.10/com/kmlab/main/ParseJSON.scala
+++ BigBoost/workspace/src/main/scala-2.10/com/kmlab/main/ParseJSON.scala
@@ -6,28 +6,96 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.jsoup._
 
-import scala.collection.mutable
+import scala.util.parsing.json.JSON
+
 
 /**
  * Created by WeiChen on 2015/12/13.
  */
 object ParseJSON {
+  def parse(rdd: RDD[String]): RDD[List[List[String]]] = {
+    val jsonMap = rdd.map(JSON.parseFull).map(_.get.asInstanceOf[scala.collection.immutable.Map[String, Any]])
+    jsonMap.map(m =>
+      m.get("words") match {
+        case Some(list) =>
+          list.asInstanceOf[List[List[String]]]
+        case None => List()
+      })
+  }
+
   def parse(rdd: RDD[String], filePath: String, outputPath: String): Unit = {
     val sc = rdd.sparkContext
     val sqlContext = new SQLContext(sc)
     val hdfsPath = "hdfs://bigboost-spark:9000/testData/word.json"
-    val fileDF = sqlContext.read.format("json").load(filePath)
-    val flattened = fileDF.explode("words", "wordInLine"){c: mutable.WrappedArray[mutable.WrappedArray[String]] => List(c: _*)}
-    val rdds = flattened.select("wordInLine").map(row => row.getAs[Seq[Seq[String]]]("wordInLine"))
-
+    val fileDF = sqlContext.read.format("json").load(hdfsPath)
+    //val flattened = fileDF.explode("words", "wordInLine") { c: mutable.WrappedArray[mutable.WrappedArray[String]] => List(c: _*) }
     //val test = fileDF.select("words").map(_.getSeq[org.apache.spark.sql.Row](0))
-
-    //fileDF.registerTempTable("JSON")
+    fileDF.registerTempTable("JSON")
     val content = sqlContext.sql("SELECT content FROM JSON").collect().map(_.toString()).map(Jsoup.parse(_).text())
     val writer = new PrintWriter(new File(outputPath))
     content.foreach { content =>
       writer.write(content)
     }
     writer.close()
+  }
+
+
+  // gen pmi for each line in each file
+  def genPmiFormJSON(rdd: RDD[List[List[String]]]): RDD[((String, String), Double)] = {
+    println("pmi ...")
+    val sc = rdd.sparkContext
+    val tmp = rdd.reduce((x, y) => x ++ y).filterNot(l => l.isEmpty) //28 sec
+    val ttmp = sc.parallelize(tmp, 500)
+    val tmpp = ttmp.reduce((x, y) => x ++ y) //28 sec
+    val fileRDD = sc.parallelize(tmpp, 500).map((_, 1))
+    val allTerms = fileRDD.reduceByKey((x, y) => x + y) //23 sec
+    val termCountMap = Map(allTerms.collect(): _*)
+    val totalWordNum = fileRDD.count().toDouble
+    sc.broadcast(termCountMap)
+    sc.broadcast(totalWordNum)
+    val list = rdd.map(article =>
+      article.map(line =>
+        line.combinations(2).filterNot(l => l(0) == l(1)).toList.map(l => ((l(0), l(1)), 1))
+      )
+    ).map(list =>
+      list.reduce((x, y) => x ++ y)
+      )
+    val tupleRDD = sc.parallelize(list.reduce((x, y) => x ++ y), 600) //45 sec
+    println("calculating coocurrence...")
+
+    val pairs = tupleRDD.reduceByKey((x, y) => x + y).map(t =>
+      ((t._1._1, termCountMap.getOrElse(t._1._1, 1)), (t._1._2, termCountMap.getOrElse(t._1._2, 1)), t._2)
+    )
+
+    println("calculating pmi...")
+    val pmi = pairs.map { pair =>
+      val x = pair._1
+      val y = pair._2
+      val pXandY = pair._3.toDouble / totalWordNum
+      val pX = x._2.toDouble / totalWordNum
+      val pY = y._2.toDouble / totalWordNum
+      val pmi = Math.log(pXandY / (pX * pY))
+      ((x._1, y._1), pmi)
+    }
+
+    val cooccurrence = tupleRDD.reduceByKey((x, y) => x + y)
+    val cccc = cooccurrence.repartition(2000)
+    val coRes = cccc.filter(t => t._2 < 5).sortBy(t => t._2)
+    coRes.saveAsTextFile("hdfs://bigboost-spark:9000/coResult/")
+
+    val ss = pmi.repartition(2000).filter(p => p._2 < 9).sortBy(t => t._2, false) //23 min !! wtf!!!
+    ss.saveAsTextFile("hdfs://bigboost-spark:9000/pmiResult/")
+
+
+
+    //ss.saveAsTextFile("/result.txt")
+    ss
+  }
+
+  def sortByPmi(rdd: RDD[((String, String), Double)], topN: Int): Array[((String, String), Double)] = {
+    rdd.top(topN)(new Ordering[((String, String), Double)]() {
+      def compare(x: ((String, String), Double), y: ((String, String), Double)): Int =
+        Ordering[Double].compare(x._2, y._2)
+    })
   }
 }
---
+++ DockerClusterInstallationGuide.md
@@ -0,0 +1,80 @@
+# Docker Cluster Installation Guide
+---
+###### 前置作業
+#### 1. Install Docker. (Follow official website.)
+```sh
+#set root password
+$ sudo passwd root
+```
+Create the docker group and add your user.
+```sh
+$ sudo usermod -aG docker username
+#log out and log back in.
+#Verify your work by running docker without sudo
+```
+
+#### 2. Install Docker machine. (https://docs.docker.com/machine/install-machine/)
+#### 3. pull swarm image
+```sh
+$ sudo docker pull swarm
+```
+
+----
+#### 來吧
+# 使用 Shipyard
+
+1. 在Master上
+```sh
+$ curl -sSL https://shipyard-project.com/deploy | bash -s
+```
+2. 各節點上
+```sh
+curl -sSL https://shipyard-project.com/deploy | ACTION=node DISCOVERY=etcd://140.119.19.231:4001 bash -s
+```
+---
+
+# 不使用Shipyard
+
+#### Swarm 管理cluster前，所有節點上 docker daemon 的port都要改成 0.0.0.0:2375
+```sh
+# 修改/etc/default/docker 
+DOCKER_OPTS="-H 0.0.0.0:2375 -H unix:///var/run/docker.sock"
+#存檔, 重開Docker
+$ sudo service docker restart
+```
+
+#### 方法1. 使用 Discovery service backend
+其中一個node可同時當swarm manager
+
+ 1. 在任何一個節點執行swarm create, 得到token
+```sh
+$ sudo docker run --rm swarm create
+```
+2. 啟動swarm manager
+```sh
+#需2375以外的port
+$ sudo docker run -d -p 2376:2375 swarm manage token://token_id
+$ docker ps #check
+```
+
+3. 在所有要加入叢集的node上面执行 swarm join 
+```sh
+#請替換ip_address與token_id
+$ sudo docker run --rm swarm join --addr=ip_address:2375 token://token_id
+```
+
+----
+### 方法2. 建立cluster list
+```sh
+echo ip:2375 >> cluster
+echo ip2:2375 >> cluster
+```
+ 在Manager上執行
+```sh
+sudo docker run -d -p 2376:2375 -v $(pwd)/cluster:/tmp/cluster swarm manage file:///tmp/cluster
+```
+----
+#### 安裝完成
+```sh
+$ docker -H 140.119.19.231:2376 run -d -p 50070:50070 --name=bigboost-sparkWorker3 --hostname=bigboost-spark --link=stormtimeseries/bigboost-spark:stormtimeseries/bigboost-spark droyuki/bigboost-spark:lab-v1 start-sparkWorker.sh
+```
